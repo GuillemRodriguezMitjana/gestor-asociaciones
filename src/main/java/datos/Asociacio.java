97e959722cf3fe0d80b6ff4c5fdbcceaeac6febf
Antoni Llop <tonichi003@gmail.com>
2024-12-09 16:38:01 +0100

    Asociacion hecha

---
+++ src/main/java/datos/Asociacio.java
@@ -0,0 +1,75 @@
+public class Asociacion {
+    private String name;
+    private String correo;
+    private String[] titulaciones; // Array para las titulaciones
+    private ListaMiembros miembros;
+    private Miembro presidente;
+    private Miembro secretario;
+    private Miembro tesorero;
+    private ListaAcciones acciones;
+
+    public Asociacion(String name, String correo, String[] titulaciones) {
+        this.name = name;
+        this.correo = correo;
+        this.titulaciones = titulaciones;
+        this.miembros = new ListaMiembros();
+        this.acciones = new ListaAcciones();    
+    }
+
+    // Métodos getters y setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCorreo() {
+        return correoo;
+    }
+
+    public void setCorreoContacto(String correo) {
+        this.correo = correo;
+    }
+
+    public String[] getTitulaciones() {
+        return titulaciones;
+    }
+
+    public void setTitulaciones(String[] titulaciones) {
+        this.titulaciones = titulaciones;
+    }
+
+    public ListaMiembros getMiembros() {
+        return miembros;
+    }
+
+    public Miembro getPresidente() {
+        return presidente;
+    }
+
+    public void setPresidente(Miembro presidente) {
+        this.presidente = presidente;
+    }
+
+    public Miembro getSecretario() {
+        return secretario;
+    }
+
+    public void setSecretario(Miembro secretario) {
+        this.secretario = secretario;
+    }
+
+    public Miembro getTesorero() {
+        return tesorero;
+    }
+
+    public void setTesorero(Miembro tesorero) {
+        this.tesorero = tesorero;
+    }
+
+    public ListaAcciones getAcciones() {
+        return acciones;
+    }
+}
