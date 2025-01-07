b2b41faf383edd394ed02d1fa3c8d5de1f7c3d1a
Antoni Llop <tonichi003@gmail.com>
2025-01-07 16:15:28 +0100

    llegit fitxer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -35,7 +35,7 @@ public class Main {
         ListaAcciones lista_acciones = new ListaAcciones();
         lista_acciones.LlegirFitxer();
         ListaMiembros lista_miembros = new ListaMiembros();
-        // lista_miembros.LlegitFitxer(); falta fer funcio
+        lista_miembros.LlegirFitxer();
 
 
         // falta cargar datos desde ficheros
