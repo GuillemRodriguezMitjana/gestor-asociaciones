436714677bd628245fb34882b393b69235a68c33
Edgar Fernández <edgar.fernandez@estudiants.urv.cat>
2025-01-07 17:26:25 +0100

    Serializable en ListaAsociaciones

--- src/main/java/listas/ListaAsociaciones.java
+++ src/main/java/listas/ListaAsociaciones.java
@@ -1,12 +1,15 @@
 package listas;
 
 import java.io.Serializable;
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.FileNotFoundException;
+import java.io.EOFException;
 
 import datos.Asociacion;
-import datos.Accion;
-import datos.Charla;
-import excepciones.ExcepcionAsociacionNoEncontrada;
 import excepciones.ExcepcionIndiceFueraDeRango;
 import excepciones.ExcepcionListaAsociacionLlena;
 
@@ -63,4 +66,37 @@ public class ListaAsociaciones implements Serializable{
         resultado += "]";
         return resultado;
     }
+    public void guardarDatos(String filename) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
+            for (int i = 0; i < nElem; i++) {
+                oos.writeObject(lista[i]);
+            }
+            System.out.println("Datos guardados correctamente en " + filename);
+        } catch (IOException e) {
+            System.out.println("Error al guardar los datos: " + e.getMessage());
+        }
+    }
+    
+    public void cargarDatos(String filename) {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
+            boolean finArchivo = false;
+            while (!finArchivo) {
+                try {
+                    Asociacion asociacion = (Asociacion) ois.readObject();
+                    agregarAsociacion(asociacion);
+                } catch (EOFException e) {
+                    finArchivo = true;
+                } catch (ExcepcionListaAsociacionLlena e) {
+                    System.out.println("La lista está llena: " + e.getMessage());
+                    break;
+                }
+            }
+            System.out.println("Datos cargados correctamente desde " + filename);
+        } catch (FileNotFoundException e) {
+            System.out.println("El archivo " + filename + " no existe.");
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Error al cargar los datos: " + e.getMessage());
+        }
+    }
 }
+
